d60d927095c09a14757ebc56cf35627dc2a1ab2d
Guest User <Guest@Epicodus-7G.local>
2017-08-04 13:40:12 -0700

    add second coupon with passing test

--- src/main/java/EventPlanner.java
+++ src/main/java/EventPlanner.java
@@ -7,15 +7,17 @@ public class EventPlanner {
     private String entertainmentOption;
     private int initialCost;
     private int couponOne;
+    private int couponTwo;
 
 
-    public EventPlanner(int guest, String meal, String beverage, String entertainment, int cost, int coupon1) {
+    public EventPlanner(int guest, String meal, String beverage, String entertainment, int cost, int coupon1, int coupon2) {
         guestNumber = guest;
         mealOption = meal;
         beverageOption = beverage;
         entertainmentOption = entertainment;
         initialCost = cost;
         couponOne = coupon1;
+        couponTwo = coupon2;
     }
 
     public int getGuest() {
@@ -39,5 +41,8 @@ public class EventPlanner {
     }
     public int getCoupon1() {
         return couponOne;
+    }
+    public int getCoupon2() {
+        return couponTwo;
     }
 }
