c41b29ba6b91c7d033ad1658890393b44ff46575
Guest User <Guest@Epicodus-7G.local>
2017-08-04 14:26:58 -0700

    show all tests passing

--- src/main/java/EventPlanner.java
+++ src/main/java/EventPlanner.java
@@ -1,6 +1,38 @@
 
 public class EventPlanner {
 
+    public int getGuestNumber() {
+        return guestNumber;
+    }
+
+    public String getMealOption() {
+        return mealOption;
+    }
+
+    public String getBeverageOption() {
+        return beverageOption;
+    }
+
+    public String getEntertainmentOption() {
+        return entertainmentOption;
+    }
+
+    public int getInitialCost() {
+        return initialCost;
+    }
+
+    public int getCouponOne() {
+        return couponOne;
+    }
+
+    public int getCouponTwo() {
+        return couponTwo;
+    }
+
+    public int getFinalCost() {
+        return finalCost;
+    }
+
     private int guestNumber;
     private String mealOption;
     private String beverageOption;
@@ -8,9 +40,10 @@ public class EventPlanner {
     private int initialCost;
     private int couponOne;
     private int couponTwo;
+    private int finalCost;
 
 
-    public EventPlanner(int guest, String meal, String beverage, String entertainment, int cost, int coupon1, int coupon2) {
+    public EventPlanner(int guest, String meal, String beverage, String entertainment, int cost, int coupon1, int coupon2, int adjustedCost) {
         guestNumber = guest;
         mealOption = meal;
         beverageOption = beverage;
@@ -18,31 +51,6 @@ public class EventPlanner {
         initialCost = cost;
         couponOne = coupon1;
         couponTwo = coupon2;
+        finalCost = adjustedCost;
     }
-
-    public int getGuest() {
-        return guestNumber;
-    }
-
-    public String getMeal() {
-        return mealOption;
-    }
-
-    public String getBeverage() {
-        return beverageOption;
-    }
-
-    public String getEntertainment() {
-        return entertainmentOption;
-    }
-
-    public int getCost() {
-        return initialCost;
-    }
-    public int getCoupon1() {
-        return couponOne;
-    }
-    public int getCoupon2() {
-        return couponTwo;
-    }
-}
+ }
--- src/test/java/EventPlannerTest.java
+++ src/test/java/EventPlannerTest.java
@@ -5,42 +5,48 @@ public class EventPlannerTest {
 
     @Test
     public void Event_instantiatesCorrectly() {
-        EventPlanner testEvent = new EventPlanner(2, "mealOption", "beverageOption", "entertainmentOption", 0, 1, 2);
+        EventPlanner testEvent = new EventPlanner(2, "mealOption", "beverageOption", "entertainmentOption", 0, 1, 2, 4);
         assertEquals(true, testEvent instanceof EventPlanner);
     }
     @Test
     public void getGuestNumber_recieveGuestCount_20() {
-        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2);
-        assertEquals(20, testEventPlanner.getGuest());
+        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2, 4);
+        assertEquals(20, testEventPlanner.getGuestNumber());
     }
     @Test
     public void getMeal_recieveMealCoice_FullDinner() {
-        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2);
-        assertEquals("FullDinner", testEventPlanner.getMeal());
+        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2, 4);
+        assertEquals("FullDinner", testEventPlanner.getMealOption() );
     }
     @Test
     public void getBeverage_recieveBeverageChoice_BeverageChoice() {
-        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2);
-        assertEquals("beverageOption", testEventPlanner.getBeverage());
+        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2, 4);
+        assertEquals("beverageOption", testEventPlanner.getBeverageOption());
     }
     @Test
     public void getEntertainment_recieveEntertainmentChoice_EntertainmentChoice() {
-        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2);
-        assertEquals(0, testEventPlanner.getCost());
+        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2, 4);
+        assertEquals("entertainmentOption", testEventPlanner.getEntertainmentOption());
     }
     @Test
     public void getTotalCost_recieveTotalCost_Cost() {
-        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2);
-        assertEquals(0, testEventPlanner.getCost());
+        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2, 4);
+        assertEquals(0, testEventPlanner.getInitialCost());
     }
     @Test
     public void getCouponOne_recieveCouponOne_Discount() {
-        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2);
-        assertEquals(0, testEventPlanner.getCost());
+        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2, 4);
+        assertEquals(1, testEventPlanner.getCouponOne());
     }
     @Test
     public void getCouponTwo_recieveCouponTwo_Discount() {
-        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2);
-        assertEquals(0, testEventPlanner.getCost());
+        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2, 4
+        );
+        assertEquals(2, testEventPlanner.getCouponTwo());
+    }
+    @Test
+    public void finalCost_recieveFinalAdjustedCost_Cost() {
+        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0, 1, 2, 4);
+        assertEquals(4, testEventPlanner.getFinalCost());
     }
 }
