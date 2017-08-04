76573e81685345d1961f16c1830e2b4500e921a7
Guest User <Guest@Epicodus-7G.local>
2017-08-04 13:28:02 -0700

    add the cost with passing test

--- src/main/java/EventPlanner.java
+++ src/main/java/EventPlanner.java
@@ -5,24 +5,34 @@ public class EventPlanner {
     private String mealOption;
     private String beverageOption;
     private String entertainmentOption;
+    private int initialCost;
 
 
-    public EventPlanner(int guest, String meal, String beverage, String entertainment) {
+    public EventPlanner(int guest, String meal, String beverage, String entertainment, int cost) {
         guestNumber = guest;
         mealOption = meal;
         beverageOption = beverage;
         entertainmentOption = entertainment;
+        initialCost = cost;
     }
-    public int getGuest()  {
+
+    public int getGuest() {
         return guestNumber;
     }
-    public String getMeal()  {
+
+    public String getMeal() {
         return mealOption;
     }
-    public String getBeverage()  {
+
+    public String getBeverage() {
         return beverageOption;
     }
-    public String getEntertainment()  {
+
+    public String getEntertainment() {
         return entertainmentOption;
+    }
+
+    public int getCost() {
+        return initialCost;
     }
 }
--- src/test/java/EventPlannerTest.java
+++ src/test/java/EventPlannerTest.java
@@ -5,27 +5,27 @@ public class EventPlannerTest {
 
     @Test
     public void Event_instantiatesCorrectly() {
-        EventPlanner testEvent = new EventPlanner(2, "mealOption", "beverageOption", "entertainmentOption");
+        EventPlanner testEvent = new EventPlanner(2, "mealOption", "beverageOption", "entertainmentOption", 0);
         assertEquals(true, testEvent instanceof EventPlanner);
     }
     @Test
     public void getGuestNumber_recieveGuestCount_20() {
-        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption");
+        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0);
         assertEquals(20, testEventPlanner.getGuest());
     }
     @Test
     public void getMeal_recieveMealCoice_FullDinner() {
-        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption");
+        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0);
         assertEquals("FullDinner", testEventPlanner.getMeal());
     }
     @Test
     public void getBeverage_recieveBeverageChoice_BeverageChoice() {
-        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption");
+        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0);
         assertEquals("beverageOption", testEventPlanner.getBeverage());
     }
     @Test
     public void getEntertainment_recieveEntertainmentChoice_EntertainmentChoice() {
-        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption");
-        assertEquals("entertainmentOption", testEventPlanner.getEntertainment());
+        EventPlanner testEventPlanner = new EventPlanner(20, "FullDinner", "beverageOption", "entertainmentOption", 0);
+        assertEquals(0, testEventPlanner.getCost());
     }
 }
