629314b3113d41a189dd87141b1a29a1fb89ea3d
Guest User <Guest@Epicodus-7G.local>
2017-08-04 10:48:26 -0700

    add final options - beverage & entertainment

--- src/main/java/EventPlanner.java
+++ src/main/java/EventPlanner.java
@@ -2,11 +2,15 @@ public class EventPlanner {
 
     private int mGuestNumber;
     private String mMealOption;
+    private String mBeverageOption;
+    private String mEntertainmentOption;
 
 
-    public EventPlanner(int guestNumber, String mealOption) {
+    public EventPlanner(int guestNumber, String mealOption, String beverageOption, String entertainmentOption) {
         mGuestNumber = guestNumber;
         mMealOption = mealOption;
+        mBeverageOption = beverageOption;
+        mEntertainmentOption = entertainmentOption;
 
     }
 
--- src/test/java/EventPlannerTest.java
+++ src/test/java/EventPlannerTest.java
@@ -5,7 +5,7 @@ public class EventPlannerTest {
 
     @Test
     public void Event_instantiatesCorrectly() {
-        EventPlanner testEvent = new EventPlanner(2, "mealOption");
+        EventPlanner testEvent = new EventPlanner(2, "mealOption", "beverageOption", "entertainmentOption");
         assertEquals(true, testEvent instanceof EventPlanner);
     }
 
