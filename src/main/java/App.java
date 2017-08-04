a5085b988d3e6de9b6a54cbce44510290753114b
Guest User <Guest@Epicodus-7G.local>
2017-08-04 16:01:04 -0700

    add first response from user input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,8 +8,19 @@ import java.io.InputStreamReader;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hi, Welcome to our event handling application. We are here to help you with all your special event handling needs.");
+        System.out.println("How many guest will be attending your event?");
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String stringNumber = bufferedReader.readLine();
+            Integer yourNumber = Integer.parseInt(stringNumber);
+            Integer yourDoubledNumber = yourNumber * 15;
 
+            System.out.println("This is out beginning price per person: $" + yourDoubledNumber + ". As we add choices, your price per person will flucuate.");
 
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
     }
-
 }
