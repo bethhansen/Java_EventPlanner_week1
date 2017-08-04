15b8e7da2c46532a267c7475639e5e87c67b0abf
Guest User <Guest@Epicodus-7G.local>
2017-08-04 10:42:38 -0700

    add second options

--- src/main/java/EventPlanner.java
+++ src/main/java/EventPlanner.java
@@ -1,10 +1,12 @@
 public class EventPlanner {
 
     private int mGuestNumber;
+    private String mMealOption;
 
 
-    public EventPlanner(int guestNumber, String foodType, String drinkType, String entertainmentType) {
+    public EventPlanner(int guestNumber, String mealOption) {
         mGuestNumber = guestNumber;
+        mMealOption = mealOption;
 
     }
 
--- src/test/java/EventPlannerTest.java
+++ src/test/java/EventPlannerTest.java
@@ -1,11 +1,11 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 
-public class EventTest {
+public class EventPlannerTest {
 
     @Test
     public void Event_instantiatesCorrectly() {
-        EventPlanner testEvent = new EventPlanner(2);
+        EventPlanner testEvent = new EventPlanner(2, "mealOption");
         assertEquals(true, testEvent instanceof EventPlanner);
     }
 
