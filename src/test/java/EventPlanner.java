59c0e6488d4db449f3da8447af85e15a8fb1d9b8
Beth <beth97209@gmail.com>
2017-08-08 21:38:32 -0700

    add updated tests

--- src/test/java/EventPlanner.java
+++ src/test/java/EventPlanner.java
@@ -1,62 +1,62 @@
-package models;
+
 
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class EventTest {
+public class EventPlanner {
 
     @Test
     public void newEventPlanner_instantiatesCorrectly() {
-        EventPlanner testEvent = new EventPlanner(2, "mealOption", "beverageOption", "entertainmentOption", 0, 0, 0, 0);
-        assertEquals(true, testEvent instanceof EventPlanner);
+        EventPlanner testEventPlanner = new EventPlanner(4, "Dinner", "Full Bar", "Live Band", "yes", "no" , "Family member", "employee" );
+        assertEquals(true, testEventPlanner instanceof EventPlanner);
     }
 
     @Test
-    public void newEventPlanner_getGuest() {
-        EventPlanner testEvent = new EventPlanner(20, "mealOption", "beverageOption", "entertainmentOption", 0, 0, 0, 0);
-        assertEquals(20, testEventPlanner.getGuest());
+    public void newEventPlanner_getGuestNumber_100() {
+        EventPlanner testEventPlanner = new EventPlanner(100, "Dinner", "Full Bar", "Live Band", "yes", "no" , "Family member", "employee" );
+        assertEquals(0, testEventPlanner.getGuestNumber());
     }
 
     @Test
-    public void newEventPlanner_getMeal_() {
-        EventPlanner testEvent
-                = new EventPlanner(20, "mealOption", "beverageOption", "entertainmentOption", 0, 0, 0, 0);
-        assertEquals("FullDinner", testEventPlanner.getMeal() );
+    public void newEventPlanner_getMealOption_Dinner() {
+        EventPlanner testEventPlanner
+                = new EventPlanner(100, "Dinner", "Full Bar", "Live Band", "yes", "no" , "Family member", "employee" );
+        assertEquals("dinner", testEventPlanner.getMealOption());
     }
 
     @Test
-    public void newEventPlanner_getBeverage_() {
-        EventPlanner testEvent = new EventPlanner(20, "mealOption", "beverageOption", "entertainmentOption", 0, 0, 0, 0);
+    public void newEventPlanner_getBeverageOption_FullBar() {
+        EventPlanner testEventPlanner = new EventPlanner(100, "Dinner", "Full Bar", "Live Band", "yes", "no" , "Family member", "employee" );
         assertEquals("beverageOption", testEventPlanner.getBeverage());
     }
 
     @Test
-    public void newEventPlanner_getEntertainment_() {
-        EventPlanner testEvent = new EventPlanner(20, "mealOption", "beverageOption", "entertainmentOption", 0, 0, 0, 0);
+    public void newEventPlanner_getEntertainmentOption_LiveBand() {
+        EventPlanner testEventPlanner = new EventPlanner(100, "Dinner", "Full Bar", "Live Band", "yes", "no" , "Family member", "employee" );
         assertEquals("entertainmentOption", testEventPlanner.getEntertainment());
     }
 
     @Test
-    public void newEventPlanner_getTotalCost_() {
-        EventPlanner testEvent = new EventPlanner(20, "mealOption", "beverageOption", "entertainmentOption", 0, 0, 0, 0);
-        assertEquals(0, testEventPlanner.getCost());
+    public void newEventPlanner_getInitialCost_1500() {
+        EventPlanner testEventPlanner = new EventPlanner(100, "Dinner", "Full Bar", "Live Band", "yes", "no" , "Family member", "employee" );
+        assertEquals(0, testEventPlanner.getInitialCost());
     }
 
     @Test
-    public void newEventPlanner_getCouponOne_() {
-        EventPlanner testEvent = new EventPlanner(20, "mealOption", "beverageOption", "entertainmentOption", 0, 0, 0, 0);
+    public void newEventPlanner_getCoupon1_familyMember() {
+        EventPlanner testEventPlanner = new EventPlanner(100, "Dinner", "Full Bar", "Live Band", "yes", "no" , "Family member", "employee" );
         assertEquals(1, testEventPlanner.getCoupon1());
     }
 
     @Test
-    public void newEventPlanner_getCouponTwo_() {
-        EventPlanner testEvent = new EventPlanner(20, "mealOption", "beverageOption", "entertainmentOption", 0, 0, 0, 0);
+    public void newEventPlanner_getCoupon2_employee() {
+        EventPlanner testEventPlanner = new EventPlanner(100, "Dinner", "Full Bar", "Live Band", "yes", "no" , "Family member", "employee" );
         assertEquals(2, testEventPlanner.getCoupon2());
     }
 
     @Test
-    public void newEventPlanner_finalCost_() {
-        EventPlanner testEvent = new EventPlanner(20, "mealOption", "beverageOption", "entertainmentOption", 0, 0, 0, 0);
+    public void newEventPlanner_getAdjustedCost_1000() {
+        EventPlanner testEventPlanner = new EventPlanner(4, "Dinner", "Full Bar", "Live Band", "yes", "no" , "Family member", "employee" );
         assertEquals(4, testEventPlanner.getAdjustedCost());
     }
 }
