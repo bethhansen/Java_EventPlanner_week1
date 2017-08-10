dfe7310f70af3e13b6636bb618ac1f7d7a311afd
Beth <beth97209@gmail.com>
2017-08-10 11:41:26 -0700

    add else if statments and remove hashmap

--- src/main/java/models/EventPlanner.java
+++ src/main/java/models/EventPlanner.java
@@ -1,73 +1,140 @@
 package models;
 
-import java.util.List;
-import java.util.HashMap;
-import java.util.Map;
-
 public class EventPlanner {
 
     private int guestNumber;
-    private String mealOption;
-    private String beverageOption;
-    private String entertainmentOption;
-    private int initialCost;
-    private int coupon1;
-    private int coupon2;
-    private int finalCost;
+    private String mealOption = "";
+    private String beverageOption = "";
+    private String entertainmentOption = "";
+    private int coupons;
+    private String coup;
+    private int price = 0;
+    private int meals;
+    private int beverages;
+    private int entertainment;
 
-    public EventPlanner(int guestNumber, String mealOption, String beverageOption, String entertainmentOption, int initialCost, int coupon1, int coupon2, int adjustedCost) {
-        this.guestNumber = guestNumber;
-        this.mealOption = mealOption;
-        this.beverageOption = beverageOption;
-        this.entertainmentOption = entertainmentOption;
-        this.initialCost = initialCost;
-        this.coupon1 = coupon1;
-        this.coupon2 = coupon2;
-        this.finalCost = adjustedCost;
+
+    // CONSTRUCTOR-----
+
+    public EventPlanner(int people, int food, int drinks, int music, int discount) {
+        guestNumber = people;
+        meals = food;
+        beverages = drinks;
+        entertainment = music;
+        coupons = discount;
+
     }
-    public int getGuestNumber() {
+
+    //GETTER-----
+
+    public int getPeople() {
         return guestNumber;
     }
+
     public String getMealOption() {
         return mealOption;
     }
+
     public String getBeverageOption() {
         return beverageOption;
     }
+
     public String getEntertainmentOption() {
         return entertainmentOption;
     }
-    public int getInitialCost() {
-        return initialCost;
+
+    public int getCoup() {
+        return coupons;
     }
-    public int getCoupon1() {
-        return coupon1;
-    }
-    public int getCoupon2() {
-        return coupon2;
-    }
-    public int getFinalCost() {
-        return finalCost;
+
+    public int getPrice() {
+        return price;
     }
 
 
+    public Integer finalCost() {
+        if (meals == 1) {
+            price += 20 * guestNumber;
+        } else if (meals == 2) {
+            price += 13 * guestNumber;
+        }
 
-    public static int calculateEventCost(int guest, String meal, String beverage, String entertainment, int cost, String coupon1, String coupon2, int finalcost) {
-        Map<String, Integer> typeValues = new HashMap<String, Integer>();
-        typeValues.put("Dinner", 20);
-        typeValues.put("Lunch", 13);
-        typeValues.put("Full Bar", 15);
-        typeValues.put("Soda", 3);
-        typeValues.put("Live Band", 300);
-        typeValues.put("DJ", 200);
-        typeValues.put("yes", 100);
-        typeValues.put("no", 0);
-        typeValues.put("Family member", 50);
-        typeValues.put("Employee", 40);
+        if (beverages == 1) {
+            price += 15 * guestNumber;
+        } else if (beverages == 2) {
+            price += 3 * guestNumber;
+        }
+
+        if (entertainment == 1) {
+            price += 300;
+        } else if (entertainment == 2) {
+            price += 200;
+        }
+
+        if (coupons == 1) {
+            price -= 50;
+        } else if (coupons == 2) {
+            price -= 40;
+        }
 
 
-        int costs = (guest * typeValues.get(meal)) + (guest * typeValues.get(beverage)) + typeValues.get(entertainment)  - typeValues.get(coupon1) - typeValues.get(coupon2);
+        return price;
+    }
 
-        return costs;
+    public String chooseMeal() {
+        if( meals == 1) {
+            mealOption = "Dinner";
+        } else if (meals == 2) {
+            mealOption = "Lunch";
+        } else {
+            mealOption = "Choose either 1 or 2.";
+        }
+        return mealOption;
+    }
+
+    public String chooseBeverage() {
+        if( beverages == 1) {
+            beverageOption = "Full Bar";
+        } else if (beverages == 2) {
+            beverageOption = "Soda";
+        } else {
+            beverageOption = "Choose either 1 or 2.";
+        }
+        return beverageOption;
+    }
+
+    public String chooseEntertainment() {
+        if( entertainment == 1) {
+            entertainmentOption = "Live Band";
+        } else if (entertainment == 2) {
+            entertainmentOption = "DJ";
+        } else {
+            entertainmentOption = "Choose either 1 or 2.";
+        }
+        return entertainmentOption;
+    }
+
+    public String chooseCoupon() {
+        if( coupons == 1) {
+            coup = "Family Member";
+        } else if (coupons == 2) {
+            coup = "Employee";
+        } else {
+            coup = "Choose 1 or 2";
+        }
+        return coup;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
